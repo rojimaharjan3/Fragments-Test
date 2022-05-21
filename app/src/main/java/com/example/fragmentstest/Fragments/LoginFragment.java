e0eada57b169364f11cbf3243fc582a0c117a065
Roji Maharjan <maharjanroji33@gmail.com>
2022-05-21 18:04:34 +0545

    Create new activity and use of intent

--- app/src/main/java/com/example/fragmentstest/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/fragmentstest/Fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragmentstest.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.fragmentstest.R;
+import com.example.fragmentstest.SecondActivity;
 
 
 public class LoginFragment extends Fragment {
@@ -19,6 +22,8 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
+    TextView textView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +32,14 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         //mainactivity maa jasari onCreate bhitra jati ni kaam garxau , tei same aba eta garna sakneyy bhayau
 
+        textView= view.findViewById(R.id.login_Text_View);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext() , SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         return view;
